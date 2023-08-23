c437a31a1cde86476182955c31888ee50fc87b5e
Heba Adel Ahmed <HebaAdelAhmedMohammed@gmail.com>
2023-08-23 05:01:36 +0300

    Commit domain layer

---
+++ lib/features/home/domain_layer/entities/book_entity.dart
@@ -0,0 +1,9 @@
+class BookEntity {
+  final String image;
+  final String title;
+  final String authorName;
+  final num rating;
+  final num price;
+
+  BookEntity(this.image, this.title, this.authorName, this.rating, this.price);
+}
